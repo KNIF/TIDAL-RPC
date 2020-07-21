f7596ee551a5ee3f9e95e32f437435edadd5384c
KNIF <admin@knif.dev>
2020-07-21 18:36:53 +0200

    Added Source Files

---
+++ TIDAL-RPC/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Timers;
+
+namespace TIDAL_RPC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                DiscordRpc.RichPresence presence = new DiscordRpc.RichPresence();
+                DiscordRpc.EventHandlers handlers = new DiscordRpc.EventHandlers();
+
+                DiscordRpc.Initialize("735159392554713099", ref handlers, true, null);
+
+                var timer = SetInterval(() => Update(ref presence), 1000);
+                timer.Start();
+
+                Console.WriteLine("RPC started.");
+
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                {
+                    timer.Stop();
+                    DiscordRpc.Shutdown();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void Update(ref DiscordRpc.RichPresence presence)
+        {
+            Process[] tidalProc = Process.GetProcessesByName("TIDAL").Where(p => p.MainWindowTitle != "").ToArray();
+
+            if (tidalProc == null || tidalProc.Length < 1)
+            {
+                presence.details = "Offline";
+                presence.state = "";
+            }
+            else
+            {
+                if (tidalProc[0].MainWindowTitle == "TIDAL")
+                {
+                    presence.details = "Idling";
+                    presence.state = "";
+                }
+                else
+                {
+                    string[] songData = tidalProc[0].MainWindowTitle.Split('-');
+
+                    string songName = songData[0];
+                    songName = songName.Remove(songName.Length - 1);
+
+                    string songArtist = songData[1];
+                    songArtist = songArtist.Substring(1);
+
+                    presence.details = songName;
+                    presence.state = songArtist;
+                }
+            }
+
+            presence.largeImageKey = "tidal";
+            presence.largeImageText = "TIDAL";
+
+            DiscordRpc.UpdatePresence(ref presence);
+        }
+
+        private static Timer SetInterval(Action Act, int Interval)
+        {
+            Timer tmr = new Timer();
+            tmr.Elapsed += (sender, args) => Act();
+            tmr.AutoReset = true;
+            tmr.Interval = Interval;
+            tmr.Start();
+
+            return tmr;
+        }
+    }
+}
---
+++ TIDAL-RPC/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TIDAL-RPC")]
+[assembly: AssemblyDescription("Discord Rich Presence for TIDAL")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KNIF")]
+[assembly: AssemblyProduct("TIDAL-RPC")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("https://knif.dev")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a8813cdb-1adf-4a26-b884-bd515b6beea9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
