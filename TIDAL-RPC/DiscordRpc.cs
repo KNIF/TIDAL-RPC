bbfa5c079bc2dc7030f8cb73414f6c40b5b1b7f0
KNIF <admin@knif.dev>
2020-07-21 18:47:36 +0200

    Fixed Formatting

--- TIDAL-RPC/DiscordRpc.cs
+++ TIDAL-RPC/DiscordRpc.cs
@@ -3,54 +3,54 @@ using System.Runtime.InteropServices;
 
 namespace TIDAL_RPC
 {
-	public class DiscordRpc
-	{
-		[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-		public delegate void ReadyCallback();
+    public class DiscordRpc
+    {
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void ReadyCallback();
 
-		[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-		public delegate void DisconnectedCallback(int errorCode, string message);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void DisconnectedCallback(int errorCode, string message);
 
-		[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-		public delegate void ErrorCallback(int errorCode, string message);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void ErrorCallback(int errorCode, string message);
 
-		public struct EventHandlers
-		{
-			public ReadyCallback readyCallback;
-			public DisconnectedCallback disconnectedCallback;
-			public ErrorCallback errorCallback;
-		}
+        public struct EventHandlers
+        {
+            public ReadyCallback readyCallback;
+            public DisconnectedCallback disconnectedCallback;
+            public ErrorCallback errorCallback;
+        }
 
-		[Serializable]
-		public struct RichPresence
-		{
-			public string state;
-			public string details;
-			public long startTimestamp;
-			public long endTimestamp;
-			public string largeImageKey;
-			public string largeImageText;
-			public string smallImageKey;
-			public string smallImageText;
-			public string partyId;
-			public int partySize;
-			public int partyMax;
-			public string matchSecret;
-			public string joinSecret;
-			public string spectateSecret;
-			public bool instance;
-		}
+        [Serializable]
+        public struct RichPresence
+        {
+            public string state;
+            public string details;
+            public long startTimestamp;
+            public long endTimestamp;
+            public string largeImageKey;
+            public string largeImageText;
+            public string smallImageKey;
+            public string smallImageText;
+            public string partyId;
+            public int partySize;
+            public int partyMax;
+            public string matchSecret;
+            public string joinSecret;
+            public string spectateSecret;
+            public bool instance;
+        }
 
-		[DllImport("discord-rpc", EntryPoint = "Discord_Initialize", CallingConvention = CallingConvention.Cdecl)]
-		public static extern void Initialize(string applicationId, ref EventHandlers handlers, bool autoRegister, string optionalSteamId);
+        [DllImport("discord-rpc", EntryPoint = "Discord_Initialize", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Initialize(string applicationId, ref EventHandlers handlers, bool autoRegister, string optionalSteamId);
 
-		[DllImport("discord-rpc", EntryPoint = "Discord_UpdatePresence", CallingConvention = CallingConvention.Cdecl)]
-		public static extern void UpdatePresence(ref RichPresence presence);
+        [DllImport("discord-rpc", EntryPoint = "Discord_UpdatePresence", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void UpdatePresence(ref RichPresence presence);
 
-		[DllImport("discord-rpc", EntryPoint = "Discord_RunCallbacks", CallingConvention = CallingConvention.Cdecl)]
-		public static extern void RunCallbacks();
+        [DllImport("discord-rpc", EntryPoint = "Discord_RunCallbacks", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void RunCallbacks();
 
-		[DllImport("discord-rpc", EntryPoint = "Discord_Shutdown", CallingConvention = CallingConvention.Cdecl)]
-		public static extern void Shutdown();
-	}
+        [DllImport("discord-rpc", EntryPoint = "Discord_Shutdown", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Shutdown();
+    }
 }
